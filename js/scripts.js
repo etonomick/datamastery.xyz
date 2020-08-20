4446469c0c322d095a647d2f6689938ea21d912f
Valery Stepanov <etonomick@gmail.com>
2020-08-20 09:15:09 +0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+let name = document.getElementById('name')
+let contact = document.getElementById('contact')
+let task = document.getElementById('task')
+let button = document.getElementById('submit')
+let message = document.getElementById('message')
+
+function submit() {
+	disable()
+	message.style.display = 'block'
+	message.innerText = 'Отправка...'
+	fetch('http://api.datamastery.xyz/task', {
+		method: 'POST',
+		cache: 'no-cache',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			name: name.value,
+			contact: contact.value,
+			task: task.value
+		})
+	}).then(data => {
+		return data.json()
+	}).then(response => {
+		enable()
+		if (response.errors.length > 0) {
+			display(response.errors[0].message, true)
+		}
+		else {
+			display('Заявка отправлена, свяжемся с вами в течение часа', false)	
+		}
+	}).catch(error => {
+		enable()
+		display(error, true)
+	})
+}
+
+function display(text, error) {
+	contact.style.border = error ? '#f44336 2px solid' : 'none'
+	button.style.display = error ? 'block' : 'none'
+	message.style.display = 'block'
+	message.style.color = error ? '#f44336' : '#4caf50'
+	message.innerText = text
+}
+
+function disable() {
+	name.setAttribute('disabled', true)
+	contact.setAttribute('disabled', true)
+	task.setAttribute('disabled', true)
+	button.setAttribute('disabled', true)
+}
+
+function enable() {
+	name.removeAttribute('disabled')
+	contact.removeAttribute('disabled')
+	task.removeAttribute('disabled')
+	button.removeAttribute('disabled')
+}
